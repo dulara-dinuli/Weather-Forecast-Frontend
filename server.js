7fc2293b0dae173e67286ea78864e34aa2f1f89b
Dulara00 <dularadinuli@gmail.com>
2024-03-03 21:45:25 +0530

    Update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 let express = require('express');
 
 let app = express();
-app.use(express.static(__dirname+'/dist/weather-forecast-frontend'));
+app.use(express.static(__dirname+'/dist/weather-forecast-frontend/browser'));
 
 app.get('/*', (req, resp)=>{
-    resp.sendFile(__dirname+'/dist/weather-forecast-frontend/index.html');
+    resp.sendFile(__dirname+'/dist/weather-forecast-frontend/browser/index.html');
 });
 
 
